4126d9d0a18e8751f0774196b93a6193df70a83f
Fabian Fagerholm <fabian.fagerholm@helsinki.fi>
2018-12-18 15:13:47 +0200

    Handle more messages

--- command-middleware/robot.js
+++ command-middleware/robot.js
@@ -48,11 +48,19 @@ const robot = {
 
 	switch(message.type) {
 	case Msg.TYPE_LIDAR_LOWRES:
+	    robot.x = message.robot_x;
+	    robot.y = message.robot_y;
+	    robot.angle = message.robot_angle;
+
+	    if (message.lidar_points.length > 0) {
+		// TODO: handle lidar points
+	    }
+
 	    return_message = "lidar_lowres";
+	    payload = robot.getStatus();
 	    break;
 	case Msg.TYPE_DBG:
 	    return_message = "debug";
-	    payload = {foo: "bar"};
 	    break;
 	case Msg.TYPE_SONAR:
 	    return_message = "sonar";
@@ -68,6 +76,13 @@ const robot = {
 	    break;
 	case Msg.TYPE_ROUTEINFO:
 	    return_message = "routeinfo";
+	    payload = {
+		start: {
+		    x: message.route_start_x,
+		    y: message.route_start_y,
+		},
+		points: message.route_points,
+	    };
 	    break;
 	case Msg.TYPE_SYNCREQ:
 	    return_message = "sync_request";
@@ -103,9 +118,48 @@ const robot = {
 	    break;
 	case Msg.TYPE_MOVEMENT_STATUS:
 	    return_message = "movement_status";
+	    payload = {
+		start: {
+		    x: message.start_x,
+		    y: message.start_y,
+		    angle: message.start_angle,
+		},
+		requested: {
+		    x: message.requested_x,
+		    y: message.requested_y,
+		    backmode: message.requested_backmode,
+		},
+		current: {
+		    x: message.current_x,
+		    y: message.current_y,
+		    angle: message.current_angle,
+		},
+		statuscode: message.statuscode,
+		success: message.success,
+		hardware_obstacle_flags: message.hardware_obstacle_flags,
+	    };
 	    break;
 	case Msg.TYPE_ROUTE_STATUS:
 	    return_message = "route_status";
+	    payload = {
+		start: {
+		    x: message.start_x,
+		    y: message.start_y,
+		    angle: message.start_angle,
+		},
+		requested: {
+		    x: message.requested_x,
+		    y: message.requested_y,
+		},
+		current: {
+		    x: message.current_x,
+		    y: message.current_y,
+		    angle: message.current_angle,
+		},
+		statuscode: message.statuscode,
+		success: message.success,
+		reroute_count: message.reroute_count,
+	    };
 	    break;
 	case Msg.STATEVECT:
 	    robot.motors_on = message.motors_on;
