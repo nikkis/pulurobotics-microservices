9b6609ba628bfb962ca6413ab93150e9f3654f42
Fabian Fagerholm <fabian.fagerholm@helsinki.fi>
2018-12-18 11:33:44 +0200

    Change SocketIO messages

--- command-middleware/robot.js
+++ command-middleware/robot.js
@@ -16,68 +16,68 @@ const robot = {
 
 	switch(message.type) {
 	case Msg.TYPE_LIDAR_LOWRES:
-	    return_message = "lidar lowres message";
+	    return_message = "lidar lowres";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_DBG:
-	    return_message = "debug message";
+	    return_message = "debug";
 	    console.log(return_message);
 	    payload = {foo: "bar"};
 	    break;
 	case Msg.TYPE_SONAR:
-	    return_message = "sonar message";
+	    return_message = "sonar";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_BATTERY:
-	    return_message = "battery message";
+	    return_message = "battery";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_ROUTEINFO:
-	    return_message = "routeinfo message";
+	    return_message = "routeinfo";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_SYNCREQ:
-	    return_message = "sync request message";
+	    return_message = "sync request";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_DBGPOINT:
-	    return_message = "dbgpoint message";
+	    return_message = "dbgpoint";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_HMAP:
-	    return_message = "hmap message";
+	    return_message = "hmap";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_INFOSTATE:
-	    return_message = "infostate message";
+	    return_message = "infostate";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_ROBOTINFO:
-	    return_message = "robotinfo message";
+	    return_message = "robotinfo";
 	    console.log(return_message);
 	    break;
 	case Msg.LIDAR_HIGHRES:
-	    return_message = "lidar highres message";
+	    return_message = "lidar highres";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_PICTURE:
-	    return_message = "picture message";
+	    return_message = "picture";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_MOVEMENT_STATUS:
-	    return_message = "movement status message";
+	    return_message = "movement status";
 	    console.log(return_message);
 	    break;
 	case Msg.TYPE_ROUTE_STATUS:
-	    return_message = "route status message";
+	    return_message = "route status";
 	    console.log(return_message);
 	    break;
 	case Msg.STATEVECT:
-	    return_message = "state vector message";
+	    return_message = "state vector";
 	    console.log(return_message);
 	    break;
 	case Msg.LOCALIZATION_RESULT:
-	    return_message = "localization result message";
+	    return_message = "localization result";
 	    console.log(return_message);
 	    break;
 	default:
