0a6e2b103f1d344fb4cf8b876b3b07b49392bd58
Fabian Fagerholm <fabian.fagerholm@helsinki.fi>
2018-12-18 12:59:57 +0200

    Test

--- command-middleware/main.js
+++ command-middleware/main.js
@@ -114,7 +114,7 @@ io.on("connection", (socket) => {
 
     clientConnectionPool[socket.id] = socket;
 
-    io.sockets.emit("robot_status", {x: 5, y: 7});
+    io.sockets.emit("robot_status", robot.getStatus());
 
     socket.on("disconnect", () => {
 	console.log(`SocketIO client id ${socket.id} disconnected.`);
