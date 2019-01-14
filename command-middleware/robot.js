63a4f9b394a845237c9f54ce3483c5aa966220d3
Fabian Fagerholm <fabian.fagerholm@helsinki.fi>
2019-01-14 11:33:01 +0200

    Bugfix

--- command-middleware/robot.js
+++ command-middleware/robot.js
@@ -191,7 +191,7 @@ const robot = {
 		    arg15 = robot.reserved8 ? 1 : 0;
 		    arg16 = robot.reserved9 ? 1 : 0;
 		    var cmd = Msg.encodeMessage(Msg.TYPE_STATEVECT_SET, "BBBBBBBBBBBBBBBB", arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16);
-		    robotSocket.write(cmd);
+		    robot.socket.write(cmd);
 		}
 		payload.waypoints_left = false;
 	    }
